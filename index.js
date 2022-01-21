4005d2910e19919d3477edcb80eff1597a6642bc
Sunny Wang <wangjinlian@hotmail.com>
2022-01-20 20:11:25 -0500

    Further clean up and comments.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+const retry = require('@lifeomic/attempt').retry;
+
 const AWS = require('aws-sdk');
 const sts = new AWS.STS();
 let iam = new AWS.IAM();
+AWS.config.update({region:'us-east-1'});
+
 const roleName = "read-admin-role"
 const roleArn = "arn:aws:iam::730508922179:role/" + roleName;
 const startTime = (new Date()).getTime();
-const retry = require('@lifeomic/attempt').retry;
+
 
 let args = process.argv.slice(2)
+const targetName = args[0]; //Name of target to be CRUDed
 
-const targetName = args[0]; //admin_role Test-Role
 console.log("Trying to handle: ", targetName, "at: ", startTime);
-AWS.config.update({region:'us-east-1'});
+
 
 async function handleTargetResource() {
 
-    //Tag role with key as "resourceName" and specific role name as its value
+    //Tag role with key as "resourceName" and specific target resource name as its value
     let res = await iam.tagRole({
         RoleName: roleName,
         Tags: [
@@ -27,7 +31,7 @@ async function handleTargetResource() {
 
     console.log("Tagging response: ", res);
 
-    //Assume role to gain permissions to read role with specific name
+    //Assume role to gain permissions to CRUD targets of specific target resource name
     res = await sts.assumeRole({
         RoleArn: roleArn,
         RoleSessionName: `role-name-${startTime}`
@@ -41,7 +45,7 @@ async function handleTargetResource() {
 
     console.log("Credentials:", JSON.stringify(options));
 
-    //Use new credentials to read new IAM role, retries with exponential backoff and jitter as there is lag for role tags to apply
+    //Use new credentials to create a new SQS queue with queue name as specific target resource name, retries with exponential backoff and jitter as there is lag for role tags to apply
     try {
         let sqs = new AWS.SQS(options);
         const data = await retry(async (context) => {
