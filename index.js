24ebd7d178cf4dd2523c1ff7c904eed4d03052df
Sunny Wang <wangjinlian@hotmail.com>
2022-02-17 19:47:03 -0500

    Adapt Lambda function for API Gateway proxy integration.

--- index.js
+++ index.js
@@ -5,45 +5,88 @@ exports.handler = async (event) => {
     const serviceRole = process.env.SECRETS_SERVICE_ROLE; 
     const resourceTaggingRole = process.env.SECRETS_TAGGING_ROLE; 
     const tableName = process.env.DYNAMODB_TABLE_NAME; 
+    const secretsProvisionerPath = process.env.SECRETS_PROVISIONER_PATH;
+    const httpMethod = event.httpMethod;
+    const path = event.path;
+    let response;
 
-    const secretsOpsController = new SecretsOpsController()
-        .setAccountId(event.accountId)
-        .setRoleName(serviceRole)
-        .setSecretName(event.secretName)
-        .setResourceTaggingRole(resourceTaggingRole)
-        .setUserName(event.userName)
-        .setPassword(event.password)
-        .setTableName(tableName);
 
-    let res = await secretsOpsController.createOrUpdateSecret();
-    res = await secretsOpsController.saveToDynamoDB(res);
+   switch(httpMethod) {
+       case "POST":
+            if(event.body == null) {
+                response = {
+                    statusCode: 400,
+                    body: "No body is found for POST http method."
+                }
+            }
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(res)
-    };
+            if(path != secretsProvisionerPath) {
+                response = {
+                    statusCode: 404,
+                    body: "Only path of " + secretsProvisionerPath + " is supported."
+                }
+            }
+
+            if(response != null) {
+                return response;
+            }
+
+            let data = JSON.parse(event.body);
+
+            const secretsOpsController = new SecretsOpsController()
+                .setAccountId(data.accountId)
+                .setRoleName(serviceRole)
+                .setSecretName(data.secretName)
+                .setResourceTaggingRole(resourceTaggingRole)
+                .setUserName(data.userName)
+                .setPassword(data.password)
+                .setTableName(tableName);
+
+            let res = await secretsOpsController.createOrUpdateSecret();
+            res = await secretsOpsController.saveToDynamoDB(res);
+
+            response = {
+                statusCode: 200,
+                body: JSON.stringify(res)
+            };
+            break;
+       default: 
+            response = {
+                statusCode: 405,
+                body: "Http Method of " + httpMethod + " is not supported."
+            }
+   }
 
     return response;
 };
 
-/*
+/* 
 //Sandbox: 975156237701 
+
+const data ={
+    "accountId": "975156237701", 
+    "secretName": "/rds/test",
+    "userName": "userName8",
+    "password": "userPassword8"
+}; 
+
 const event = {
-  "accountId": "730508922179", 
-  "secretName": "/rds/admin3",
-  "userName": "userName7",
-  "password": "userPassword7"
+    "path": "/secrets",
+    "httpMethod": "POST",
+    "body": JSON.stringify(data)
 }; 
 
 exports.handler(event).then(res=>{
     console.log("test", res);
 });
-*/
+ */
 
 /*
 export SECRETS_SERVICE_ROLE=Secrets-Provisioning-Role
 export SECRETS_TAGGING_ROLE=Secrets-Tagging-Role
 export DYNAMODB_TABLE_NAME=SecretsInfo
+export SECRETS_PROVISIONER_PATH=/secrets
+
 zip -r function.zip . 
 aws lambda update-function-code --function-name SecretsProvisioner --zip-file fileb://function.zip
 */
