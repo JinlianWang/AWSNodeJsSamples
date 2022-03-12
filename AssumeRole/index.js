a227dac83bddb0fb02a1d13d5d2b2929ba4725f1
Sunny Wang <wangjinlian@hotmail.com>
2022-03-12 16:01:00 -0500

    Demo request object event listener to look into signature v4.

--- AssumeRole/index.js
+++ AssumeRole/index.js
@@ -20,12 +20,39 @@ async function assumeRole() {
 async function getIdentity(credentials) {
     const sts = new AWS.STS(credentials);
     const res = await sts.getCallerIdentity({}).promise();
-    console.log("Identity: " + JSON.stringify(res));
+    return res; 
 }
 
-const f = async ()=> {
+async function getIdentityWithEventListener(credentials) {
+    const sts = new AWS.STS(credentials);
+    const request = sts.getCallerIdentity({});
+    request.on("sign", function (req) {
+        console.log("iam_http_request_method:", req.httpRequest.method);
+        console.log("iam_request_url:", req.httpRequest.endpoint.href);
+        console.log("iam_request_headers:", JSON.stringify(req.httpRequest.headers));
+        console.log("iam_request_body:", JSON.stringify(req.httpRequest.body));
+        req.abort();//abort so that we only get the signed headers. 
+    });
+
+    request.on("send", function (res) {
+        console.log("Hmm, 'send' event shall not fire as the request is aborted: " + res.httpResponse.statusCode + " body: " + res.httpResponse.body);
+    });
+
+    request.on('success', function (res) {
+        console.log("Success!");
+        console.log("Response: " + res.httpResponse.statusCode + " body: " + res.httpResponse.body);
+    }).on('error', function (err) {
+        console.log("Error: " + JSON.stringify(err));
+    }).on('complete', function () {
+        console.log("Always!");
+    }).send();
+}
+
+const f = async () => {
     const credentials = await assumeRole();
-    getIdentity(credentials);
+    const res = await getIdentity(credentials);
+    console.log("Identity: " + JSON.stringify(res));
+    getIdentityWithEventListener(credentials);
 };
 
 f();
